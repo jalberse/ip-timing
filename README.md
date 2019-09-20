3ecc801fccb405a77e209c29df970bafb77bedb9
jalberse <alberse.john@gmail.com>
2019-09-19 19:12:11 -0500

    Error handling and documentation

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # IP Timing
 
-Test timing for GCP deploy regions.
+Test timing for GCP deploy regions. This project requires pyyaml, which we include in our Pipfile.
 
-Run `pipenv install` to install the virtual environment. Run `pipenv shell` to enter it. 
+Run `pipenv install` to install the virtual environment. Run `pipenv shell` to enter it.
+
+Run the code with `python timing.py [filename]` where `[filename]` is a .yaml file of the format:
+
+```
+---
+  ips:
+    - IP_ONE
+    - IP_TWO
+    - IP_N
+```
+
+For an arbitrary number N of IPs.
+
+This will print out a tab-delimited list of (ip,elapsed) tuples where 'elapsed' is the wall clock time it took for a request to finish. Pipe this output to a file if you wish to do further analysis etc.
--- timing.py
+++ timing.py
@@ -1,8 +1,21 @@
 import yaml
+import requests
+import time
+import sys
 
 if __name__ == "__main__":
-    with open('ips.yaml','r') as f:
-        doc = yaml.safe_load(f)
-        for ip in doc['ips']:
-            # TODO test time for each IP and output
-            print(ip)
+    if (len(sys.argv) != 2):
+        print("Pass in a .yaml file. See README.md")
+        exit()
+    try:
+        with open(sys.argv[1],'r') as f:
+            doc = yaml.safe_load(f)
+            for ip in doc['ips']:
+                start = time.time()
+                requests.get(ip) # requests is blocking
+                stop = time.time()
+                elapsed = stop - start
+                print(ip + "    " + str(elapsed))
+    except FileNotFoundError as fnf_error:
+            print(fnf_error)
+            exit()
